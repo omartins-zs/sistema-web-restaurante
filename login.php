a3c988979978a4f21eec86854776f3c41c574175
omartins-zs <gabrielmatheus209@gmail.com>
2021-11-23 12:36:24 -0300

    Pegando e Verificando se email e senha esta no BD

--- login.php
+++ login.php
@@ -1,17 +1,40 @@
 <?php
-// 1. Pegar os valores do formulario
 
-// 2. Conexao com banco de dados 
-$servername = "localhost";
-$username = "root";
-$password = "";
+if (count($_POST) > 0) {
+    // 1. Pegar os valores do formulario
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=restaurante-bd", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Conexão realizada com sucesso";
-} catch(PDOException $e) {
-  echo "CConexão falhou: " . $e->getMessage();
+    // 2. Conexao com banco de dados 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=restaurante-bd", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //   echo "Conexão realizada com sucesso";
+
+        // 3. Verifica se email e senha estao no bando de dados
+        $stmt = $conn->prepare("SELECT codigo FROM usuario WHERE email=:email AND senha=md5(:senha)");
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->bindParam(':senha', $senha, PDO::PARAM_STR);
+        $stmt->execute();
+
+        // set the resulting array to associative
+        $result = $stmt->fetchAll();
+        $qtd_usuarios = count($result);
+        if ($qtd_usuarios == 1) {
+            // TODO substituido pelo redirecionamento
+            $resultado["msg"] = "Usuario Encontrado!";
+            $resultado["cod"] = 1;
+        } else if ($qtd_usuarios == 0) {
+            $resultado["msg"] = "Email e Senha nao conferem...";
+            $resultado["cod"] = 0;
+        }
+    } catch (PDOException $e) {
+        echo "Conexão falhou: " . $e->getMessage();
+    }
+    $conn = null;
 }
-// 3. Verificar se email e senha estao no bando de dados
+include("index.php");
