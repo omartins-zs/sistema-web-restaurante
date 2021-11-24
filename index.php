9b55de22d97e807d357205a1230adffec8f5b3be
omartins-zs <gabrielmatheus209@gmail.com>
2021-11-24 11:13:45 -0300

    Inserindo conteudo no container

--- index.php
+++ index.php
@@ -8,27 +8,28 @@
     <!-- CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-
     <title>Registro de Pedidos 1.0</title>
 </head>
 
 <body>
-    <h2>Efetue login</h2>
-    <form action="login.php" id="form_login" method="POST">
-        <?php if (isset($resultado) && $resultado["cod"] == 0) : ?>
-            <div class="alert alert-danger">
-                <?php echo $resultado["msg"]; ?>
-            </div>
-        <?php endif; ?>
-        <input id="email" name="email" type="email" placeholder="Digite seu email">
-        <br><br>
-        <input id="senha" name="senha" type="password" placeholder="Digite sua senha">
-        <br><br>
-        <input type="submit" id="submeter" value="Entrar">
-    </form>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <div class="container">
+        <h2>Efetue login</h2>
+        <form action="login.php" id="form_login" method="POST">
+            <?php if (isset($resultado) && $resultado["cod"] == 0) : ?>
+                <div class="alert alert-danger">
+                    <?php echo $resultado["msg"]; ?>
+                </div>
+            <?php endif; ?>
+            <input id="email" name="email" type="email" placeholder="Digite seu email">
+            <br><br>
+            <input id="senha" name="senha" type="password" placeholder="Digite sua senha">
+            <br><br>
+            <input type="submit" id="submeter" value="Entrar" class="btn btn-primary">
+        </form>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </div>
 </body>
 
 </html>
