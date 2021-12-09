e056474625a1bde1fa343f4a65eb3617bb2975ce
omartins-zs <gabrielmatheus209@gmail.com>
2021-12-09 10:25:04 -0300

    1° Refatoração Function Atualizar Produto

--- alterar_produto.php
+++ alterar_produto.php
@@ -1,32 +1,12 @@
 <?php
 
 if (count($_POST) > 0) {
-    // 1. Pegar os valores do formulario
-    $nome = $_POST["nome_produto"];
-    $categoria = $_POST["categoria_produto"];
-    $valor = $_POST["valor_produto"];
-    $foto = $_POST["foto_produto"];
-    $info = $_POST["info_produto"];
-    $cod_prod = $_POST["cod_prod"];
 
-    try {
-        // 2. Conexao com banco de dados 
-        include("conexao_bd.php");
+    require_once('produto/Produto.php');
 
-        // 3. Inseri os dados no BD
-        $sql = "UPDATE produto SET nome = ?, categoria = ?, valor = ?, info_adicional = ?, data_hora = now() WHERE codigo = ?";
-        $stmt = $conn->prepare($sql); // STMT = Consulta
-        $stmt->execute([$nome, $categoria, $valor, $info, $cod_prod]);
+    $produto = new Produto();
 
-        // TODO substituido pelo redirecionamento
-        $resultado["msg"] = "Item alterado com sucesso!";
-        $resultado["cod"] = 1;
-        $resultado["style"] = "alert-success";
-    } catch (PDOException $e) {
-        $resultado["msg"] = "Erro ao inserir produto no banco de dados: " . $e->getMessage();
-        $resultado["cod"] = 0;
-        $resultado["style"] = "alert-danger";
-    }
-    $conn = null;
+    $resultado = $produto->atualizar($_POST);
+
+    header("Location: produto.php");
 }
-header("Location: produto.php");
--- produto/Produto.php
+++ produto/Produto.php
@@ -14,6 +14,7 @@ class Produto
     {
         session_start();
 
+        $this->codigo = $$valores["cod_prod"];
         $this->nome = $$valores["nome_produto"];
         $this->categoria = $$valores["categoria_produto"];
         $this->valor = $$valores["valor_produto"];
@@ -22,9 +23,9 @@ class Produto
         $this->cod_usuario = $_SESSION["codigo_usuario"];
     }
 
-    function inserir($valores)
+    function inserir($produto)
     {
-        $this->pegar_valores_post($valores);
+        $this->pegar_valores_post($produto);
 
         try {
             // 2. Conexao com banco de dados 
@@ -35,6 +36,33 @@ class Produto
             $stmt->execute([$this->nome, $this->categoria, $this->valor, $this->foto, $this->info, $this->cod_usuario]);
 
             $resultado["msg"] = "Produto inserido com sucesso!";
+            $resultado["cod"] = 1;
+            $resultado["style"] = "alert-success";
+        } catch (PDOException $e) {
+            $resultado["msg"] = "Erro ao inserir produto no banco de dados: " . $e->getMessage();
+            $resultado["cod"] = 0;
+            $resultado["style"] = "alert-danger";
+        }
+        $conn = null;
+
+        return $resultado;
+    }
+
+    function atualizar($produto)
+    {
+        $this->pegar_valores_post($produto);
+
+        try {
+            // 2. Conexao com banco de dados 
+            include("conexao_bd.php");
+
+            // 3. Inseri os dados no BD
+            $sql = "UPDATE produto SET nome = ?, categoria = ?, valor = ?, info_adicional = ?, data_hora = now() WHERE codigo = ?";
+            $stmt = $conn->prepare($sql); // STMT = Consulta
+            $stmt->execute([$this->nome, $this->categoria, $this->valor, $this->info, $this->codigo]);
+
+            // TODO substituido pelo redirecionamento
+            $resultado["msg"] = "Item alterado com sucesso!";
             $resultado["cod"] = 1;
             $resultado["style"] = "alert-success";
         } catch (PDOException $e) {
