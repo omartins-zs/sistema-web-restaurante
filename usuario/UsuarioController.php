bfa72ba99ebc0383e01b2fbd21447df1b22bd3b8
omartins-zs <gabrielmatheus209@gmail.com>
2021-12-16 09:35:43 -0300

    Criando Function Login

--- usuario/UsuarioController.php
+++ usuario/UsuarioController.php
@@ -18,4 +18,30 @@ class UsuarioController
     {
         $resultado = $this->usuario->inserir($valores);
     }
+    function login($valores)
+    {
+        // 1. Pegar os valores do formulario
+        $filtro = array();
+        $filtro["email"] = $valores["email"];
+        $filtro["senha"] = $valores["senha"];
+        $filtro["situacao"] = "Habilitado";
+
+        $usuario = $this->usuario->selecionar($filtro);
+
+
+        if (count($usuario) == 1) {
+            session_start();
+            $_SESSION["email_usuario"] =
+                $valores["email"];
+            $_SESSION["nome_usuario"] = $usuario[0]["nome"];
+            $_SESSION["codigo_usuario"] = $usuario[0]["codigo"];
+
+            header("Location: produto.php");
+        } else if (count($usuario) == 0) {
+            $resultado["msg"] = "Email e Senha nao conferem...";
+            $resultado["cod"] = 0;
+
+            return $resultado;
+        }
+    }
 }
